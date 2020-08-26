dfb911e33b3be9a0debdf57b874273ba47031c81
deepok1@yahoo.com <deepok1@yahoo.com>
2020-08-26 10:02:05 +0300

    Updates

---
+++ precache-manifest.06a572165eae1ad05eb4fb4220832b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce19fa2155c9524873427b8d66b8fe9",
+    "url": "/crazy-app/index.html"
+  },
+  {
+    "revision": "2bd281c54e4235ac816e",
+    "url": "/crazy-app/static/css/main.a4d7dfd3.chunk.css"
+  },
+  {
+    "revision": "36c613a8254c5e88e705",
+    "url": "/crazy-app/static/js/2.939c6194.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crazy-app/static/js/2.939c6194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd281c54e4235ac816e",
+    "url": "/crazy-app/static/js/main.122cbc6b.chunk.js"
+  },
+  {
+    "revision": "10086e2ffc134db2e8a5",
+    "url": "/crazy-app/static/js/runtime-main.dc225a28.js"
+  }
+]);
